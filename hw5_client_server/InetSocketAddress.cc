5aa949f7ed08671dcc0dfddd460eee3bf4e38d8f
Vadim Brilyantov <v.brilyantov@yandex.ru>
2018-05-07 00:30:35 +0300

    libnet changed to posix lib

--- hw5_client_server/InetSocketAddress.cc
+++ hw5_client_server/InetSocketAddress.cc
@@ -2,7 +2,7 @@
 // Created by Vadim on 14/04/2018.
 //
 
-#include <libnet.h>
+#include <arpa/inet.h>
 #include <utility>
 #include "InetSocketAddress.h"
 #include <future>
@@ -23,6 +23,5 @@ SystemSocketAddress InetSocketAddress::ToSystemSocketAddress() const {
   system_socket_address.sin_family = AF_INET;
   system_socket_address.sin_port = htons(port);
   inet_pton(AF_INET, host.c_str(), &system_socket_address.sin_addr);
-  assert(system_socket_address.sin_addr.s_addr == htonl(INADDR_LOOPBACK));
   return system_socket_address;
 }
