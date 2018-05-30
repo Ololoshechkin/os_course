fbadf923aa00afb4c991173cb92302b4549d0b10
Vadim Brilyantov <v.brilyantov@yandex.ru>
2018-05-30 22:08:55 +0000

    cmake updated (3.5 version support)

---
+++ IPC/build/CMakeFiles/IPC.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/os_course/IPC/ExceptionHelp.cc" "/os_course/IPC/build/CMakeFiles/IPC.dir/ExceptionHelp.cc.o"
+  "/os_course/IPC/KVClient.cc" "/os_course/IPC/build/CMakeFiles/IPC.dir/KVClient.cc.o"
+  "/os_course/IPC/KVDatabaseServer.cc" "/os_course/IPC/build/CMakeFiles/IPC.dir/KVDatabaseServer.cc.o"
+  "/os_course/IPC/ScopedPipe.cc" "/os_course/IPC/build/CMakeFiles/IPC.dir/ScopedPipe.cc.o"
+  "/os_course/IPC/ScopedUnixSocket.cc" "/os_course/IPC/build/CMakeFiles/IPC.dir/ScopedUnixSocket.cc.o"
+  "/os_course/IPC/ZygoteDatabaseService.cc" "/os_course/IPC/build/CMakeFiles/IPC.dir/ZygoteDatabaseService.cc.o"
+  "/os_course/IPC/main.cpp" "/os_course/IPC/build/CMakeFiles/IPC.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
